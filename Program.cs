a7bda9a63a407b3e3f8f3f9ea80eab4ceb73c860
midnight33 <gra33@bk.ru>
2023-02-28 19:25:24 +0300

    Задача 5

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 5
+
+int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
